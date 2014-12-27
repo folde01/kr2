a33582ad63584c0e9bd2205c8cb84e3abbe00b3e
jesse <folde01@dcs.bbk.ac.uk>
2014-12-27 12:20:10 +0000

    strcat seems to work

--- 0ch5-pointers-and-arrays/strcat.c
+++ 0ch5-pointers-and-arrays/strcat.c
@@ -2,6 +2,24 @@
 #include <string.h>
 
 
+void strcat3(char *s, char *t) 
+{ 
+  //while (*s++ != '\0') { 
+  printf("*s: %s s: %d\n", s, s);
+  //while (*s++ != '\0') { 
+  while (*s++) { 
+    printf("*s: %s s: %d\n", s, s);
+  }
+  printf("*s: %s s: %d\n", s, s);
+  printf("%s\n", "yoda");
+  s--;
+  while (*s++ = *t++) { 
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+
+  }
+}
+
 /* strcat: concatenate t to end of s; s must be big enough */
 void strcat2(char s[], char t[])
 { 
@@ -14,9 +32,16 @@ void strcat2(char s[], char t[])
       ;
 }
 
+
+
 main() { 
   char s[] = "abc";
   char t[] = "defg";
-  strcat(s, t);
-  printf("%s\n", s);
+  strcat2(s, t);
+  printf("%s\n\n", s);
+
+  char s3[] = "av";
+  char t3[] = "iii";
+  strcat3(s3, t3);
+  printf("%s\n", s3);
 }

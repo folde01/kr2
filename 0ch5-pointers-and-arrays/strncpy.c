f704e6e012583960278755c3d9f9acf31266c5fd
jesse <folde01@dcs.bbk.ac.uk>
2014-12-31 13:43:42 +0000

    how to get rid of type warning

---
+++ 0ch5-pointers-and-arrays/strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/* copy at most n characters of string ct to s ; return s . Pad with '\0' characters if ct has fewer than n characters. */
+//char *strncpy2(s, ct, n);
+
+main() { 
+  char s[] = "abcd";
+  char ct[] = "efg";
+  int n = 2; 
+  printf("efcd ? %s\n", strncpy(s, ct, n));
+
+  char s2[] = "abcdef";
+  char ct2[] = "hi";
+  int n2 = 4; 
+  char *res = strncpy(s2, ct2, n2);
+  printf("hi???? ? %s\n", res);
+
+  while (*res) { 
+    printf("res: %d\n", res);
+    ++res;
+  }
+
+}
+

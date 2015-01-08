4ffbed05540fd185962e36e67e7444f2ee236113
J O <folde01@bbk.ac.uk>
2015-01-08 09:20:45 +0000

    strncat.c: forgot about 'at most n characters'. Added now.

--- 0ch5-pointers-and-arrays/strncat.c
+++ 0ch5-pointers-and-arrays/strncat.c
@@ -6,15 +6,21 @@ char *strncat2(char *s, char *ct, int n);
 
 char *strncat2(char *s, char *ct, int n) { 
   char *p = s;
+  /*
   while (*s) { 
     printf("yoda: %s\n", s);
     s++;
   }
-  while (*ct) { 
+  */
+  s += strlen(s);
+  int i = 0;
+  while (*ct && i < n) { 
     printf("hamish: %s\n", s);
+    ++i;
     *s++ = *ct++;
   }
   *s = '\0';
+
   return p;
 }
 
@@ -25,7 +31,7 @@ main() {
   printf("abcde ? %s\n", strncat(s1, ct1, n1));   
 
   char s2[44] = "abc";
-  char ct2[3] = "de";
-  int n2 = 2;
-  printf("abcde ? %s\n", strncat2(s2, ct2, n2));   
+  char ct2[3] = "def";
+  int n2 = 3;
+  printf("abcde ? %s\n", strncat2(s2, ct2, n2));   // is it null terminating? How to test for this? Check each char?
 }

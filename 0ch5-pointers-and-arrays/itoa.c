be5d87b4e360889a777c2c4b5885de7fd748abd9
J O <folde01@bbk.ac.uk>
2015-01-19 09:30:30 +0000

    itoa: started

--- 0ch5-pointers-and-arrays/itoa.c
+++ 0ch5-pointers-and-arrays/itoa.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 
 /* reverse: reverse string s in place */
 void reverse(char s[])
@@ -29,4 +31,30 @@ void itoa(int n, char s[])
     s[i++] = '-';
   s[i] = '\0';
   reverse(s);
+}
+
+
+
+
+void itoa2(int n, char s[]) { 
+  char *p = s;
+  do { 
+    s* =  
+  } while (n /= 10 > 0)
+  reverse_p(p);
+}
+
+void main() { 
+  // test 1:
+  int lim = 10;
+  char s[lim];
+  int n = 123;
+  itoa(n, s);
+  printf("itoa: %s\n", s);
+
+  // test 2:
+  char s[lim];
+  int n2 = 1234;
+  itoa2(n2, s2);
+  printf("itoa2: %s\n", s2);
 }

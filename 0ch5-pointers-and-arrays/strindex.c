e0defe986c126dfbb99cc3c0f5f76d70bb1e8b8b
J O <folde01@bbk.ac.uk>
2015-01-24 15:24:09 +0000

    strindex.c: getting closer?

--- 0ch5-pointers-and-arrays/strindex.c
+++ 0ch5-pointers-and-arrays/strindex.c
@@ -12,17 +12,18 @@ int strindex(char s[], char t[])
   }
   return -1;
 }
-
-int strindex_p(char *s, char *t) { 
-  char *s0 = s;
-  char *t0 = t;
-  while (s = s0 && *s0 != '\0') { 
-    while(t += (s - s0) && *t == *s) { 
-      ++t;
+int strindex_p(char *s, char *t) {  
+  char *s0;
+  char *t0;
+  while (*s++ != '\0') { 
+    s0 = s; 
+    t0 = t;
+    while (*s0 == *t0) { 
+      printf("yoda\n");
+      ++s0, ++t0;
     }
-    if (*t == *s && *t == '\0')
-      return s - s0;  
-    ++s0;
+    if (*t0 == '\0' && t0 > t)
+      return s0-s;
   }
   return -1;
 }
@@ -38,4 +39,14 @@ main()
   char *s2 = "taxi";
   char *t2 = "ax";
   printf("1 ... %d\n", strindex_p(s2, t2));
+
+  // test 3
+  char *s3 = "abcdefg";
+  char *t3 = "def";
+  printf("3 ... %d\n", strindex_p(s3, t3));
+
+  // test 4
+  char *s4 = "abcdefg";
+  char *t4 = "e";
+  printf("4 ... %d\n", strindex_p(s4, t4));
 }
